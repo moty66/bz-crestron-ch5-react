e6d6d1ed1b67c39a91deb47f515e0dd6d3b248ac
Motaz Abuthiab <moty66@gmail.com>
2024-03-25 10:46:02 +0100

    adding tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './assets/App.css'
-import { useState, useEffect, useMemo } from 'react';
-import useWebXPanel from './hooks/useWebXPanel';
+import "./assets/App.css";
+import { useState, useEffect, useMemo } from "react";
+import useWebXPanel from "./hooks/useWebXPanel";
 
 // Initialize eruda for panel/app debugging capabilities (in dev mode only)
-if (import.meta.env.VITE_APP_ENV === 'development') {
-  import('eruda').then(({ default: eruda }) => {
+if (import.meta.env.VITE_APP_ENV === "development") {
+  import("eruda").then(({ default: eruda }) => {
     eruda.init();
   });
 }
@@ -14,12 +14,16 @@ function App() {
   const [analogState, setAnalogState] = useState(0);
   const [serialState, setSerialState] = useState("");
 
-  const webXPanelConfig = useMemo(() => ({
-    ipId: '0x03',
-    host: '0.0.0.0',
-    roomId: '',
-    authToken: ''
-  }), []); // Dependencies array is empty, so this object is created only once
+  const webXPanelConfig = useMemo(
+    () => ({
+      ipId: "0x06",
+      host: "192.168.1.123",
+      roomId: "",
+      authToken:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsIlNvdXJjZSI6IkNvbnRyb2wgU3lzdGVtIn0.eyJleHAiOjE3MTEwMzAxNDQsInVzZXJuYW1lIjoiT2ZmbGluZVRva2VuIiwiT3B0aW9uYWwiOiI1MjIyODEwMzQifQ.2IalK01LXz5bzexqVFQAuZ9VrCSzJFJNNiop-dtHOVs",
+    }),
+    []
+  ); // Dependencies array is empty, so this object is created only once
 
   useWebXPanel(webXPanelConfig);
 
@@ -28,38 +32,68 @@ function App() {
     // d1Id, a1Id, and s1Id are the subscription IDs for each join, they do not
     // carry any values and are only used to unsubscribe from the join when the
     // component unmounts
-    const d1Id = window.CrComLib.subscribeState('b', '1', (value: boolean) => setDigitalState(value));
-    const a1Id = window.CrComLib.subscribeState('n', '1', (value: number) => setAnalogState(value));
-    const s1Id = window.CrComLib.subscribeState('s', '1', (value: string) => setSerialState(value));
+    const d1Id = window.CrComLib.subscribeState("b", "1", (value: boolean) =>
+      setDigitalState(value)
+    );
+    const a1Id = window.CrComLib.subscribeState("n", "1", (value: number) =>
+      setAnalogState(value)
+    );
+    const s1Id = window.CrComLib.subscribeState("s", "1", (value: string) =>
+      setSerialState(value)
+    );
 
     return () => {
       // Unsubscribe from digital, analog, and serial joins 1 when component unmounts
-      window.CrComLib.unsubscribeState('b', '1', d1Id);
-      window.CrComLib.unsubscribeState('n', '1', a1Id);
-      window.CrComLib.unsubscribeState('s', '1', s1Id);
-    }
+      window.CrComLib.unsubscribeState("b", "1", d1Id);
+      window.CrComLib.unsubscribeState("n", "1", a1Id);
+      window.CrComLib.unsubscribeState("s", "1", s1Id);
+    };
   }, []);
 
   // Send digital, analog, and serial 1 joins to the control system
-  const sendDigital = (value: boolean) => window.CrComLib.publishEvent('b', '1', value);
-  const sendAnalog = (value: number) => window.CrComLib.publishEvent('n', '1', value);
-  const sendSerial = (value: string) => window.CrComLib.publishEvent('s', '1', value);
+  const sendDigital = (value: boolean) =>
+    window.CrComLib.publishEvent("b", "1", value);
+  const sendAnalog = (value: number) =>
+    window.CrComLib.publishEvent("n", "1", value);
+  const sendSerial = (value: string) =>
+    window.CrComLib.publishEvent("s", "1", value);
 
   return (
     <div id="controlGroupWrapper">
       <div className="controlGroup">
-        <button id="sendDigitalButton" className="btn" onClick={() => sendDigital(!digitalState)}>Toggle Digital</button>
+        <button
+          id="sendDigitalButton"
+          className="btn"
+          onClick={() => sendDigital(!digitalState)}
+        >
+          Toggle Digital
+        </button>
         <p id="currentDigitalValue">{digitalState.toString()}</p>
       </div>
       <div className="controlGroup">
-          <p id="currentAnalogValue">{analogState}</p>
-          <input type="range" min="0" max="65535" value={analogState} placeholder="32767" id="analogSlider" onChange={(e) => sendAnalog(Number(e.target.value))} />
+        <p id="currentAnalogValue">{analogState}</p>
+        <input
+          type="range"
+          min="0"
+          max="65535"
+          value={analogState}
+          placeholder="32767"
+          id="analogSlider"
+          onChange={(e) => sendAnalog(Number(e.target.value))}
+        />
       </div>
       <div className="controlGroup">
-          <input type="text" name="Data" id="currentSerialValue" placeholder="Placeholder" value={serialState} onChange={(e) => sendSerial(e.target.value)} />
+        <input
+          type="text"
+          name="Data"
+          id="currentSerialValue"
+          placeholder="Placeholder"
+          value={serialState}
+          onChange={(e) => sendSerial(e.target.value)}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  const isDevelopment = env.VITE_APP_ENV === 'development';
+  const isDevelopment = env.VITE_APP_ENV === "development";
 
   return defineConfig({
     plugins: [
@@ -14,21 +14,21 @@ export default ({ mode }) => {
       viteStaticCopy({
         targets: [
           {
-            src: 'node_modules/@crestron/ch5-crcomlib/build_bundles/umd/cr-com-lib.js',
-            dest: ''
+            src: "node_modules/@crestron/ch5-crcomlib/build_bundles/umd/cr-com-lib.js",
+            dest: "",
           },
           {
-            src: 'node_modules/@crestron/ch5-webxpanel/dist/umd/index.js',
-            dest: ''
+            src: "node_modules/@crestron/ch5-webxpanel/dist/umd/index.js",
+            dest: "",
           },
           {
-            src: 'node_modules/@crestron/ch5-webxpanel/dist/umd/d4412f0cafef4f213591.worker.js',
-            dest: ''
-          }
-        ]
-      })
+            src: "node_modules/@crestron/ch5-webxpanel/dist/umd/d4412f0cafef4f213591.worker.js",
+            dest: "",
+          },
+        ],
+      }),
     ],
-    base: './',
+    base: "./",
     build: {
       sourcemap: isDevelopment,
     },
